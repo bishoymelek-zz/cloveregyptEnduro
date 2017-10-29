526187b29308de4bff92bd0c8169b13485485aaf
bishoy <bishoy.wadie@ambulancers.com>
2017-10-29 17:19:20 +0500

    first commet

---
+++ cms/.users.js
@@ -0,0 +1,11 @@
+{
+	users: [
+		{
+			username: 'bishoymwadie',
+			tags: [],
+			salt: '6dc298cbba3691a75a858d4bd8ac587f',
+			hash: '96a4044af0faeb8452cb6a238d3d3bc0718fbd28a2ae4175be765748005d55ed',
+			user_created_timestamp: 1509189846190
+		}
+	]
+}
---
+++ cms/index.js
@@ -0,0 +1,99 @@
+{
+	AboutUsTitle: 'About CloverEgypt',
+	aboutUsDescribtion: 'We are the biggest and most promising in the region!',
+	aboutUsServices: [
+		{
+			label: 'first'
+		},
+		{
+			label: 'second'
+		}
+	],
+	projectManagerName: 'Mina Zaki',
+	projectManagerImage: 'https://pbs.twimg.com/profile_images/460045304377524224/zelN27NU.jpeg',
+	projectManagerDetails: 'lorem',
+	projectManagerFacebook: '',
+	projectManagerSkype: '',
+	projectManagerLinkedin: '',
+	ourServices: [
+		{
+			serviceTitle: 'designs gamdaaaaa',
+			serviceDetails: 'e7na gamdiiin awi'
+		},
+		{
+			serviceTitle: 'designs gamdaaaaa',
+			serviceDetails: 'e7na gamdiiin awi'
+		},
+		{
+			serviceTitle: 'designs gamdaaaaa',
+			serviceDetails: 'e7na gamdiiin awi'
+		},
+		{
+			serviceTitle: 'designs gamdaaaaa',
+			serviceDetails: 'e7na gamdiiin awi'
+		},
+		{
+			serviceTitle: 'designs gamdaaaaa',
+			serviceDetails: 'e7na gamdiiin awi'
+		},
+		{
+			serviceTitle: 'designs gamdaaaaa',
+			serviceDetails: 'e7na gamdiiin awi'
+		}
+	],
+	testimonials: [
+		{
+			testimonialsImg: 'https://cdn.thinglink.me/api/image/782615334380109826/1240/10/scaletowidth',
+			testimonialsDetails: 'el kalam da gamed awi',
+			testimonialsName: 'bill gates',
+			testimonialsJob: 'wa7d tnin'
+		}
+	],
+	portfolioItems: [
+		{
+			portfolioItemLink: 'google.coms',
+			portfolioItemImg: 'https://cdn.thinglink.me/api/image/782615334380109826/1240/10/scaletowidth',
+			portfolioItemTitle: 'Agmd job',
+			portfolioItemDetails: 'e7na hna zabtna el kalam 3l a5eeeeeer'
+		}
+	],
+	facts: [
+		{
+			factsImage: 'https://cdn.thinglink.me/api/image/782615334380109826/1240/10/scaletowidth',
+			factsNumber: '652',
+			factsTitle: 'projoects'
+		}
+	],
+	teamMember: [
+		{
+			teamMemberImage: 'https://cdn.thinglink.me/api/image/782615334380109826/1240/10/scaletowidth',
+			teamMemberName: 'Steve Jobs',
+			teamMemberJob: 'Agmd wa7d',
+			teamMemberFacebook: '',
+			teamMemberTwitter: ''
+		}
+	],
+	contactUsLng: '30.6565',
+	contactUsLat: '29.3265',
+	infoWorkingHours: '',
+	infoPhone: '056465654',
+	copyrightText: '',
+	logos: [
+		{
+			logoImage: 'http://pngimg.com/uploads/car_logo/car_logo_PNG1667.png'
+		},
+		{
+			logoImage: 'http://pngimg.com/uploads/car_logo/car_logo_PNG1667.png'
+		},
+		{
+			logoImage: 'http://pngimg.com/uploads/car_logo/car_logo_PNG1667.png'
+		}
+	],
+	socialMediaTwitter : '',
+	socialMediaFacebook : '',
+	socialMediaGoogle :'',
+	socialMediaLinkedin : '',
+	socialMediaSkype: '',
+	socialMediaInstagram: '',
+	socialMediaYoutube: ''
+}
