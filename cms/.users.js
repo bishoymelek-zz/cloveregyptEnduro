9bf2b9186acc88099cf93fc06244742bccd13d48
bishoy <lydianaeem95@gmail.com>
2018-01-26 22:42:07 +0200

    add Account mina

--- cms/.users.js
+++ cms/.users.js
@@ -6,6 +6,13 @@
 			salt: '6dc298cbba3691a75a858d4bd8ac587f',
 			hash: '96a4044af0faeb8452cb6a238d3d3bc0718fbd28a2ae4175be765748005d55ed',
 			user_created_timestamp: 1509189846190
+		},
+		{
+			username: 'mina',
+			tags: [],
+			salt: '1e19e7b714576e75e33257c91d551db5',
+			hash: 'a90606a241e9f6e16b2c42832b8a36066aec0e5a10e32fa0e939a3e45f250991',
+			user_created_timestamp: 1516999271999
 		}
 	]
 }
